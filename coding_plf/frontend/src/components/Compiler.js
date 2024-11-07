0c2c8d8ef173e8a67b9130e005717a997a55e09c
Shaurya Pandey <127183365+Shaurya4136@users.noreply.github.com>
2024-11-07 19:36:43 +0530

    Update Compiler.js

--- coding_plf/frontend/src/components/Compiler.js
+++ coding_plf/frontend/src/components/Compiler.js
@@ -17,7 +17,7 @@ const CodeEditor = ({ languages, initialLanguage = 'python3', onRun }) => {
 
   const handleRunCode = async () => {
     try {
-      const response = await fetch('http://localhost:5000/run', {
+      const response = await fetch('https://finalprj-backend.onrender.com/run', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
